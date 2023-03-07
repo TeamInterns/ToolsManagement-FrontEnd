858f794ac1f7715929ece2f2ceb410ae4bfb9729
Shailja Bhaskar <71603660+ShailjaBhaskar@users.noreply.github.com>
2023-03-07 10:34:50 +0530

    Pushing changes

--- src/Login.js
+++ src/Login.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {useNavigate} from 'react-router-dom'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
@@ -7,9 +7,15 @@ import {Row, Col, Image} from 'react-bootstrap';
 
 const Login = () => {
   const navigate = useNavigate();
+  const [email, setEmail]=useState('');
+    const [password, setPassword]=useState('');
 
   const handleLogin=(e)=>{
     e.preventDefault();
+    console.log(email);
+    console.log(password);
+    setEmail('');
+    setPassword('');
     setTimeout(()=>{
       navigate('/');
   },3000)
@@ -31,12 +37,12 @@ const Login = () => {
       <Form style={{width:"80%", marginLeft:"10%", marginTop:"10%"}} onSubmit={handleLogin}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Enter email" required onChange={(e)=>setEmail(e.target.value)} value={email}/>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" placeholder="Password" required onChange={(e)=>setPassword(e.target.value)} value={password}/>
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
@@ -49,7 +55,7 @@ const Login = () => {
         </Col>
         
         <Col >
-        <div style={{width:"80%", marginRight:"10%", marginTop:"10%"}}>
+        <div style={{width:"80%", marginRight:"10%", marginTop:"20%"}}>
            <Image style={{width:"100%"}} src="https://img.freepik.com/free-vector/job-interview-conversation_74855-7566.jpg?w=996&t=st=1677682708~exp=1677683308~hmac=723981cd2fa17a8357b560e4526fc1fcade77dabd945a4273570fbfcebfab9f5"  /> 
         </div>
         </Col>
--- src/Signup.js
+++ src/Signup.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {useNavigate} from 'react-router-dom'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import {Row, Col, Image} from 'react-bootstrap';
 
+const options = ["User", "Manager", "Admin"];
+
 const Signup = () => {
   const navigate = useNavigate()
 
+    const [fullName, setFullname]=useState('');
+    const [email, setEmail]=useState('');
+    const [password, setPassword]=useState('');
+    const [role, setRole]=useState(options[0]);
+
   const handleSignup=(e)=>{
     e.preventDefault();
+    console.log('Signup Successfull. You will now automatically get redirected to Login');
+    console.log(fullName)
+    console.log(email)
+    console.log(password)
+    console.log(role)
+    setFullname('');
+    setEmail('');
+    setPassword('');
     setTimeout(()=>{
       navigate('/login');
   },3000)
@@ -29,23 +44,29 @@ const Signup = () => {
            <Form style={{width:"80%", marginLeft:"10%", marginTop:"10%"}} onSubmit={handleSignup}>
            <Form.Group className="mb-3" controlId="formBasicName">
         <Form.Label>User Name</Form.Label>
-        <Form.Control type="text" placeholder="Full Name" />
+        <Form.Control type="text" placeholder="Full Name" required
+                onChange={(e)=>setFullname(e.target.value)} value={fullName}/>
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Enter email" required
+                 onChange={(e)=>setEmail(e.target.value)} value={email}/>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" placeholder="Password" required
+                 onChange={(e)=>setPassword(e.target.value)} value={password}/>
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicPassword">
       <Form.Label>Role</Form.Label>
-      <Form.Select aria-label="Default select example">
-      <option value="1">User</option>
-      <option value="2">Manager</option>
-      <option value="3">Admin</option>
+      <Form.Select aria-label="Default select example" value={role} 
+       onChange={e => setRole(e.target.value)}>
+      {options.map((value) => (
+          <option value={value} key={value}>
+            {value}
+          </option>
+        ))}
     </Form.Select>
     </Form.Group>
       <Button variant="primary" type="submit">
@@ -59,7 +80,7 @@ const Signup = () => {
        </Col>
         
         <Col >
-        <div style={{width:"80%", marginRight:"10%", marginTop:"10%"}}>
+        <div style={{width:"80%", marginRight:"10%", marginTop:"20%"}}>
            <Image style={{width:"100%"}} src="https://img.freepik.com/free-vector/job-interview-conversation_74855-7566.jpg?w=996&t=st=1677682708~exp=1677683308~hmac=723981cd2fa17a8357b560e4526fc1fcade77dabd945a4273570fbfcebfab9f5"  /> 
         </div>
         </Col>
